c8d61668e9796ab23b64b07846c44dd7276a5c9e
Olivier Chafik <olivier.chafik@gmail.com>
2011-08-30 14:50:05 +0100

    JNAerator: Updated date in source copyright notices

--- jna-jnaerator/src/main/java/com/sun/jna/BitFields.java
+++ jna-jnaerator/src/main/java/com/sun/jna/BitFields.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright (c) 2009 Olivier Chafik, All Rights Reserved
+ * Copyright (c) 2009-2011 Olivier Chafik, All Rights Reserved
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
