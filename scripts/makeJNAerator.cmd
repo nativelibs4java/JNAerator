04b0c8b7f93a363192a7c257cfad3a66661fe940
Olivier Chafik <olivier.chafik@gmail.com>
2011-10-08 18:40:25 +0100

    Moved scripts around

--- scripts/makeJNAerator.cmd
+++
@@ -1,7 +0,0 @@
-MAVEN_OPTS=-Xmx500m
-#-Dmaven.compiler.debug=false
-MVN_NOTEST="mvn -Djarsigner.skip=false -Dstorepass=$KEYSTORE_PASS -Dmaven.test.skip=true" 
-$MVN_NOTEST clean install
-$MVN_NOTEST deploy
-
-#mvn -f jnaerator-runtime/pom.xml clean deploy install && mvn "-Dstorepass=$KEYSTORE_PASS" -f jnaerator/pom.xml clean deploy site-deploy
